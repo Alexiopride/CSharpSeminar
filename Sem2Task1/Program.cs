792051d47c93743206698bc4977c8bc89145741e
alexiopride <alexiopride@gmail.com>
2022-09-09 12:36:15 +0400

    Added tasks 1,2,3

--- Sem2Task1/Program.cs
+++ Sem2Task1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу которая выводит случайное число
+//  из отрезка (10, 99) и показывает наибольшую
+//  цифру числа.
+
+
+// int randomNum = new Random().Next(10, 100);
+// Console.WriteLine(randomNum);
+// int digitFirst = randomNum / 10;
+// int digitSecond = randomNum % 10;
+// Console.WriteLine($"{digitFirst}, {digitSecond}");
+
+
+int randomNum = new Random().Next(10, 100);
+Console.WriteLine(randomNum);
+int digitFirst = randomNum / 10;
+int digitSecond = randomNum % 10;
+Console.WriteLine(FindMax(digitFirst, digitSecond));
+int FindMax(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        return num1;
+    }
+    return num2;
+}
